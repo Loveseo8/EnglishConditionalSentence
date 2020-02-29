531b6fdbf03cbbbf2d4be602c19fec33b108ee42
Loveseo8 <58880849+Loveseo8@users.noreply.github.com>
2020-02-29 17:40:45 +0300

    Update ConditionalIdentifier.java

--- src/com/company/ConditionalIdentifier.java
+++ src/com/company/ConditionalIdentifier.java
@@ -12,26 +12,25 @@ public class ConditionalIdentifier {
 
     public void identify(String words[]) {
         for (int i = 0; i < words.length; i++) {
-            if (words[i].contains("had")) {
+            if (words[i].contains("have") || words[i].contains("haven’t") || words[i].contains("has") || words[i].contains("hasn’t")){
                 for (i = 0; i < words.length; i++) {
-                    if(words[i].contains("have")){
-                for (i = 0; i < words.length; i++) {
-                    if (words[i].contains("would") || words[i].contains("should") || words[i].contains("must") || words[i].contains("could") || words[i].contains("might") || words[i].contains("may")) {
-                        //out.print("III");
-                        a = 3;
+                    if (words[i].contains("would") || words[i].contains("wouldn’t") || words[i].contains("could") || words[i].contains("couldn’t") || words[i].contains("might") || words[i].contains("mightn’t") || words[i].contains("must") || words[i].contains("mustn’t") || words[i].contains("should") || words[i].contains("shouldn’t")) {
+                        for (i = 0; i < words.length; i++) {
+                            if (words[i].contains("had") || words[i].contains("hadn’t")) {
+                                a = 3;
 
+                            }
+                        }
                     }
                 }
-            }
-        }
-            } else if (words[i].contains("would") || words[i].contains("should") || words[i].contains("must") || words[i].contains("could") || words[i].contains("might") || words[i].contains("may")) {
-                //out.print("II");
+            } else if (words[i].contains("would") || words[i].contains("wouldn’t") || words[i].contains("could") || words[i].contains("couldn’t") || words[i].contains("might") || words[i].contains("mightn’t") || words[i].contains("must") || words[i].contains("mustn’t") || words[i].contains("should") || words[i].contains("shouldn’t")) {
                 a = 2;
-            } else if (words[i].contains("will") || words[i].contains("ll")|| words[i].contains("should") || words[i].contains("can") || words[i].endsWith("ing") || words[i].contains("shall")) {
-                //out.print("I");
+
+            } else if (words[i].contains("will") || words[i].endsWith("’ll") || words[i].contains("won’t") || words[i].contains("can") || words[i].contains("can’t") || words[i].contains("may") || words[i].contains("mayn’t") || words[i].contains("must") || words[i].contains("mustn’t") || words[i].contains("should") || words[i].contains("shouldn’t")) {
                 a = 1;
             }
         }
+
 
         switch (a){
             case 0: out.println(0);
